761ce8d20d07c5e8d50e60cd1a64db30341e7c5d
Lucas Sales <lucas.sales95@hotmail.com>
2019-02-13 03:03:32 -0300

    Instalei e configurei o PostCSS

--- webpack.dev.js
+++ webpack.dev.js
@@ -39,6 +39,13 @@ module.exports = {
 			},
 
 			test: /\.js$/
+		}, {
+			test: /\.css$/,
+			use: [
+				'style-loader',
+				{ loader: 'css-loader', options: { importLoaders: 1 } },
+				'postcss-loader'
+			]
 		}]
 	},
 
